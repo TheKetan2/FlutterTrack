9097840751cdbb651f7272057bd12e8fa5df0c39
Ketan <ketantoo@gmail.com>
2019-05-17 02:05:00 +0530

    Mi Card App done

--- mi_card_flutter-master/lib/main.dart
+++ mi_card_flutter-master/lib/main.dart
@@ -11,16 +11,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('img/namo.jpg'),
+              ),
+              Text(
+                'Ketan D. Ramteke',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FULLSTACK DEVELOPER',
+                style: TextStyle(
+                  color: Colors.teal[100],
+                  fontSize: 20.0,
+                  letterSpacing: 3.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text('2222222222',
+                          style: TextStyle(
+                            color: Colors.teal[900],
+                            fontFamily: 'SansPro',
+                            fontSize: 20.0,
+                          )),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListTile(
+                    leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text('ketan@ramteke.com',
+                          style: TextStyle(
+                            color: Colors.teal[900],
+                            fontFamily: 'SansPro',
+                            fontSize: 20.0,
+                          )),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+
