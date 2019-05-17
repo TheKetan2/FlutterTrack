9eabdf0dbb060b29f1be7e1fe74f4091ef46d7c6
Ketan <ketantoo@gmail.com>
2019-05-18 04:27:53 +0530

    Ramdom image generator functionality added

--- angela_dice_flutter/lib/main.dart
+++ angela_dice_flutter/lib/main.dart
@@ -1,44 +1,58 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main(){
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
+void main() {
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Center(child: Text('Dicee')),
         backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Center(child: Text('Dicee')),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
       ),
-    ));
+      body: DicePage(),
+    ),
+  ));
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int righDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
     return Center(
-          child: Row(
+      child: Row(
         children: <Widget>[
-         Expanded(
-           child: FlatButton(
-             child: Image.asset('images/dice1.png'),
-             onPressed: (){
-               print('Left Button Pressed');
-             },
-             ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = new Random().nextInt(6)+1;
+                });
+
+                print('Left Button Pressed $leftDiceNumber');
+              },
+            ),
           ),
-         Expanded(
-           child: FlatButton(
-             onPressed: (){
-               print('Right button got pressed.');
-             },
-             child: Image.asset('images/dice2.png')
-           ),
-         ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                      righDiceNumber = new Random().nextInt(6)+1;
+                  });
+                  
+                  print('Right button got pressed.');
+                },
+                child: Image.asset('images/dice$righDiceNumber.png')),
+          ),
         ],
       ),
     );
   }
 }
-
