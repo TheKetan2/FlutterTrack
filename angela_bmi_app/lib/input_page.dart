3f4cced4cf0a4ec01890a19e630eef4d8a0dcc2c
Ketan <ketantoo@gmail.com>
2019-05-26 03:12:01 +0530

    added fontawesome package

--- angela_bmi_app/lib/input_page.dart
+++ angela_bmi_app/lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const btmHeight = 80.0;
+const Color coulor = Color(0xff1d1e33);
+const Color btmColor = Color(0xffeb1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,7 +11,6 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,23 +23,63 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                    child: ResusableCard(Color(0xff1d1eaa))),
+                    child: ResusableCard(
+                        clr: coulor,
+                        cardChild: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.mars, size: 80.0),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'MALE',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Color(0xff8d8e98),
+                              ),
+                            ),
+                          ],
+                        ))),
                 Expanded(
-                    child: ResusableCard(Color(0xff1d1e33))),
+                    child: ResusableCard(
+                        clr: coulor,
+                        cardChild: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Icon(FontAwesomeIcons.venus, size: 80.0),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'MALE',
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Color(0xff8d8e98),
+                              ),
+                            ),
+                          ],
+                        ))),
               ],
             ),
           ),
           Expanded(
-              child: ResusableCard(Color(0xff1d1e33))),
+              child: ResusableCard(
+            clr: coulor,
+          )),
           Expanded(
             child: Row(
               children: <Widget>[
-                Expanded(
-                    child: ResusableCard(Color(0xff1d1e33))),
-                Expanded(
-                    child: ResusableCard(Color(0xff1d1e33))),
+                Expanded(child: ResusableCard(clr: coulor)),
+                Expanded(child: ResusableCard(clr: coulor)),
               ],
             ),
+          ),
+          Container(
+            color: btmColor,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: btmHeight,
           ),
         ],
       ),
@@ -43,20 +87,18 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-
-
 class ResusableCard extends StatelessWidget {
-
-  ResusableCard(@required this.clr);
-  Color clr;
+  ResusableCard({@required this.clr, this.cardChild});
+  final Color clr;
+  final Widget cardChild;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                      color: clr,
-                      borderRadius: BorderRadius.circular(10.0)),
-                );
+      child: cardChild,
+      margin: EdgeInsets.all(15.0),
+      decoration:
+          BoxDecoration(color: clr, borderRadius: BorderRadius.circular(10.0)),
+    );
   }
 }
