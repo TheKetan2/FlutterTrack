928f9de2891e13e5ff3fbddd68b38cc240ba5b7c
Ketan <ketantoo@gmail.com>
2019-05-25 17:29:17 +0530

    Created BMI app layout

--- angela_bmi_app/lib/input_page.dart
+++ angela_bmi_app/lib/input_page.dart
@@ -10,13 +10,59 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('BMI CALCULATOR'))
-        ,
+        title: Center(child: Text('BMI CALCULATOR')),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xff1d1e33),
+                      borderRadius: BorderRadius.circular(10.0)),
+                )),
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xff1d1e33),
+                      borderRadius: BorderRadius.circular(10.0)),
+                )),
+              ],
+            ),
+          ),
+          Expanded(
+              child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color: Color(0xff1d1e33),
+                borderRadius: BorderRadius.circular(10.0)),
+          )),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xff1d1e33),
+                      borderRadius: BorderRadius.circular(10.0)),
+                )),
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xff1d1e33),
+                      borderRadius: BorderRadius.circular(10.0)),
+                )),
+              ],
+            ),
+          ),
+        ],
       ),
-     
     );
   }
 }
