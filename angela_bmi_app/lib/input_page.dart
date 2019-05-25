e4bb3910e5f5cdcbc2da78d555b90bd8c906671e
Ketan <ketantoo@gmail.com>
2019-05-25 17:07:46 +0530

    Created BMI app

---
+++ angela_bmi_app/lib/input_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('BMI CALCULATOR'))
+        ,
+      ),
+      body: Center(
+        child: Text('Body Text'),
+      ),
+     
+    );
+  }
+}
---
+++ angela_bmi_app/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        
+        primaryColor: Color(0xff0a0e21),
+        scaffoldBackgroundColor: Color(0xff0a0e21),
+      ),
+      home: InputPage(),
+    );
+  }
+}
+
