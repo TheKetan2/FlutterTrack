5d74caf48b5dc42e18fdfa07ccd59de7fe3e2086
Ketan <ketantoo@gmail.com>
2019-05-26 06:12:18 +0530

    enum

--- angela_bmi_app/lib/input_page.dart
+++ angela_bmi_app/lib/input_page.dart
@@ -6,6 +6,13 @@ import 'reusable_card.dart';
 const btmHeight = 80.0;
 const Color coulor = Color(0xff1d1e33);
 const Color btmColor = Color(0xffeb1555);
+const Color activeColor = Color(0xff1d1e33);
+const Color inactiveColor = Color(0xff111328);
+
+enum Gender{
+  male,
+  female
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,6 +20,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,14 +35,35 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                    child: ResusableCard(
-                        clr: coulor, cardChild: new IconContent(gender: 'Male',
-                        iconData: FontAwesomeIcons.mars,))),
+                    child: GestureDetector(
+                  onTap: () {
+                    print("Tapped");
+                    setState(() {
+                      selectedGender = Gender.male;
+                    });
+                    
+                  },
+                  child: ResusableCard(
+                      clr: selectedGender == Gender.male?activeColor:inactiveColor,
+                      cardChild: new IconContent(
+                        gender: 'Male',
+                        iconData: FontAwesomeIcons.mars,
+                      )),
+                )),
                 Expanded(
-                    child: ResusableCard(
-                        clr: coulor,
-                        cardChild: new IconContent(gender: 'Female',
-                        iconData: FontAwesomeIcons.venus,))), 
+                    child: GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                  child: ResusableCard(
+                      clr: selectedGender == Gender.female?activeColor:inactiveColor,
+                      cardChild: new IconContent(
+                        gender: 'Female',
+                        iconData: FontAwesomeIcons.venus,
+                      )),
+                )),
               ],
             ),
           ),
@@ -59,5 +90,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-
