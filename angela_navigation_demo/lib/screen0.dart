3aaa3e84825fa11d1f21b0343ae14a34a038ca01
Ketan <ketantoo@gmail.com>
2019-05-30 04:06:21 +0530

    Navigation app demo

--- angela_navigation_demo/lib/screen0.dart
+++ angela_navigation_demo/lib/screen0.dart
@@ -16,6 +16,7 @@ class Screen0 extends StatelessWidget {
               child: Text('Go To Screen 1'),
               onPressed: () {
                 //Navigate to Screen 1
+                Navigator.pushNamed(context, '/first');
               },
             ),
             RaisedButton(
@@ -23,6 +24,7 @@ class Screen0 extends StatelessWidget {
               child: Text('Go To Screen 2'),
               onPressed: () {
                 //Navigate to Screen 2
+                Navigator.pushNamed(context, '/second');
               },
             ),
           ],
