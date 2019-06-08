51c7f1b979babef0da60a073ba498d36195e323a
Ketan <ketantoo@gmail.com>
2019-06-09 02:20:13 +0530

    Hero animation

--- angela_firebase_chat/lib/screens/login_screen.dart
+++ angela_firebase_chat/lib/screens/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-
   static String id = 'login_screen';
   @override
   _LoginScreenState createState() => _LoginScreenState();
@@ -18,9 +17,12 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
+            Hero(
+              tag: 'logo',
+              child: Container(
+                height: 200.0,
+                child: Image.asset('images/logo.png'),
+              ),
             ),
             SizedBox(
               height: 48.0,
