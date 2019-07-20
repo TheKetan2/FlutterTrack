3805b0610b0f980a6925e56cb45d931bf205af93
Ketan <ketantoo@gmail.com>
2019-07-20 23:33:23 +0530

    Had to restart again tq vs code for ruining

--- quiz_app_new/lib/main.dart
+++ quiz_app_new/lib/main.dart
@@ -5,8 +5,32 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = ['Your fav animal?', 'Your fav color?'];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Quiz App',
+          ),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The Question:'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
