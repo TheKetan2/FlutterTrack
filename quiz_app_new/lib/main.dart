de32737738b2e1eb5bb8cd9c6aecc8d998889d2a
Ketan <ketantoo@gmail.com>
2019-07-26 13:28:52 +0530

    26 July

--- quiz_app_new/lib/main.dart
+++ quiz_app_new/lib/main.dart
@@ -2,10 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+
+}
+
+class MyAppState extends State<MyApp>{
+  var questionIndex = 0;
+   ansQuestion(){
+      print("Answer chosen.");
+      print(questionIndex);
+      setState(() {
+       questionIndex++; 
+      });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    var questions = ['Your fav animal?', 'Your fav color?'];
+    var questions = ['Your fav animal?', 'Your fav color?','Your fav animal?', 'Your fav color?','Your fav animal?', 'Your fav color?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -15,18 +35,18 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The Question:'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: null,
+              onPressed: ansQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: null,
+              onPressed: () => print('Ans 2 pressed'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: null,
+              onPressed: () => print('Ans 3 pressed'),
             ),
           ],
         ),
