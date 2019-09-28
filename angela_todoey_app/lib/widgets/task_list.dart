ec6fff7b063119c785a7963a505b680ea607a672
Ketan <ketantoo@gmail.com>
2019-09-28 21:08:33 +0530

    added tasks modals and listviewbuilder

--- angela_todoey_app/lib/widgets/task_list.dart
+++ angela_todoey_app/lib/widgets/task_list.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 import 'task_tile.dart';
+import '../modals/task.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
+  @override
+  _TaskListState createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemCount: tasks.length,
+      itemBuilder: (ctx, i) => TaskTile(
+          taskTitle: tasks[i].name,
+          isChecked: tasks[i].isDone,
+          checkboxCallback: (bool checkboxState) {
+            setState(() {
+              tasks[i].toggleDone();
+            });
+          }),
     );
   }
 }
--- angela_todoey_app/lib/widgets/task_tile.dart
+++ angela_todoey_app/lib/widgets/task_tile.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkboxCallback;
+  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
       trailing: Checkbox(
-        value: false,
-        onChanged: null,
+        value: isChecked,
+        activeColor: Colors.lightBlueAccent,
+        onChanged: checkboxCallback,
       ),
     );
   }
