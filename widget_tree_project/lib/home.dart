3eda5cfd7242271ea201d716b3033ce5418b6992
Ketan <ketantoo@gmail.com>
2019-12-04 13:18:44 +0530

    creating WidgetTree and making shallow tree by refractoring nested widgets into methods and Widget classes

--- widget_tree_project/lib/home.dart
+++ widget_tree_project/lib/home.dart
@@ -11,13 +11,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Container buildContainer(double h, double w, Color c) {
-    return Container(
-                      height: h,
-                      width: w,
-                      color: c,
-                    );
-  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,66 +22,13 @@ class _HomeState extends State<Home> {
           child: Column(
             
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-              
-                  buildContainer(40, 40, Colors.yellow),
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                  ),
-                  Expanded(
-                    child: buildContainer(40, 40, Colors.amber)
-                    
-                  ),
-                  Padding(padding: EdgeInsets.all(16),),
-
-                  buildContainer(40, 40, Colors.red),
-                  Padding(padding: EdgeInsets.all(16.0),),
-                  Row(
-// mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        children: <Widget>[
-                          
-                          buildContainer(60, 60, Colors.blue),
-                          Padding(padding: EdgeInsets.all(16),),
-                         
-                          buildContainer(40, 40, Colors.amber),
-                          Padding(
-                            padding: EdgeInsets.all(16),
-                          ),
-                          
-                          buildContainer(40, 40, Colors.brown,),
-                          Padding(padding: EdgeInsets.all(16),),
-                          
-                          buildContainer(20, 20, Colors.brown),
-                          Divider(),
-                          Row(children: <Widget>[
-                            CircleAvatar(
-                              backgroundColor: Colors.lightGreen,
-                              radius: 100,
-                              child: Stack(
-                                children: <Widget>[
-                                  buildContainer(100, 100, Colors.yellow),
-                                  buildContainer(60, 60, Colors.amber),
-                                  buildContainer(40, 40, Colors.brown),
-                                ],
-                              ),
-                            )
-                          ],),
-                          Divider(),
-                          Text("End of the Line")
-                        ],
-                      )
-                    ],
-                  ),
-                ],
+              new HorizontalRow(),
+              Padding(padding: EdgeInsets.all(16)),
+              new VerticlaWidget(),
+              ],
               )
-            ],
-          ),
+            
+          
 
         ),
       ),
@@ -97,3 +38,96 @@ class _HomeState extends State<Home> {
 
   
 }
+
+class VerticlaWidget extends StatelessWidget {
+  const VerticlaWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            
+            buildContainer(60, 60, Colors.blue),
+            Padding(padding: EdgeInsets.all(16),),
+           
+            buildContainer(40, 40, Colors.amber),
+            Padding(padding: EdgeInsets.all(16),),
+            
+            buildContainer(40, 40, Colors.brown,),
+            Padding(padding: EdgeInsets.all(16),),
+            
+            buildContainer(20, 20, Colors.brown),
+            Divider(),
+            new StackWidget(),
+            Divider(),
+            Text("End of the Line")
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class StackWidget extends StatelessWidget {
+  const StackWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      CircleAvatar(
+        backgroundColor: Colors.lightGreen,
+        radius: 100,
+        child: Stack(
+          children: <Widget>[
+            buildContainer(100, 100, Colors.yellow),
+            buildContainer(60, 60, Colors.amber),
+            buildContainer(40, 40, Colors.brown),
+          ],
+        ),
+      )
+    ],);
+  }
+}
+
+class HorizontalRow extends StatelessWidget {
+  const HorizontalRow({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+    
+        buildContainer(40, 40, Colors.yellow),
+        Padding(
+          padding: EdgeInsets.all(16),
+        ),
+        Expanded(
+          child: buildContainer(40, 40, Colors.amber)
+          
+        ),
+        Padding(padding: EdgeInsets.all(16),),
+
+        buildContainer(40, 40, Colors.red),
+        Padding(padding: EdgeInsets.all(16.0),),],
+        );
+  }
+}
+
+Container buildContainer(double h, double w, Color c) {
+    return Container(
+                      height: h,
+                      width: w,
+                      color: c,
+                    );
+  }
